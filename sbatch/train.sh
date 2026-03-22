9c6a0dd97e743fcea8fe26c522a66102c9d8c2b7
Xinyu Sheng <sheng.xin.yu@foxmail.com>
2026-03-22 19:21:45 +0800

    [add] 新增训练脚本 train.sh，配置训练环境和分布式训练参数

--- sbatch/train.sh
+++ sbatch/train.sh
@@ -13,12 +13,14 @@ export NCCL_SOCKET_IFNAME=eno2
 export NCCL_DEBUG=INFO
 
 # 激活环境
-source /mnt/slurmfs-4090node1/homes/xsheng420/miniforge3/bin/activate instinct
+source /mnt/slurmfs-4090node1/homes/xsheng420/miniconda3/bin/activate instinct
 
-# 简化版：省略MASTER_PORT/ADDR，由torchrun+srun自动推导
-srun torchrun \
-  --nproc_per_node=4 \
-  scripts/instinct_rl/train.py \
+# 使用srun启动分布式训练
+srun python -m torch.distributed.run \
+  --nnodes=1  --nproc_per_node=4 \
+  ../scripts/instinct_rl/train.py \
   --task=Instinct-Parkour-Target-Amp-G1-v0 \
+  --distributed \
+  --enable_cameras \
   --num_envs=1024 \
-  --distributed
+  
